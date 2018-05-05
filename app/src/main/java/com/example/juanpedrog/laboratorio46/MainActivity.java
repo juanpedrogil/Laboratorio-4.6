b265cc3592f1ba9333182ca8322969dc4e367952
Juan Pedro Gil Llanos <jupegilll@ittepic.edu.mx>
2018-05-04 18:32:00 -0700

    Primer commit

---
+++ app/src/main/java/com/example/juanpedrog/laboratorio46/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.juanpedrog.laboratorio46;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private AnimationDrawable monkeyAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.animation);
+        if (imageView == null) throw new AssertionError();
+
+        imageView.setVisibility(View.INVISIBLE);
+        imageView.setBackgroundResource(R.drawable.espio_animation);
+
+        monkeyAnimation = (AnimationDrawable) imageView.getBackground();
+        monkeyAnimation.setOneShot(true);
+    }
+
+    public void onStartBtnClick(View v) {
+        imageView.setVisibility(View.VISIBLE);
+        if (monkeyAnimation.isRunning()) {
+            monkeyAnimation.stop();
+        }
+        monkeyAnimation.start();
+    }
+
+    public void onStopBtnClick(View v) {
+        monkeyAnimation.stop();
+    }
+}
